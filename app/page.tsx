bc5155c6512b9c611aa05b1ea7aba068cb8494ad
sahebbali <sahebbali253@gmail.comm>
2023-06-10 12:06:55 +0600

    Reservation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,44 @@
 import Image from 'next/image'
+import getListings, {IListingsParams} from './actions/getListings';
+import getCurrentUser from './actions/getCurrentUser';
+import ClientOnly from '@/components/ClientOnly';
+import EmptyState from '@/components/EmptyState';
+import Container from '@/components/Conitainer';
+import ListingCard from '@/components/listings/ListingCard';
 
-export default function Home() {
+
+interface HomeProps {
+  searchParams: IListingsParams;
+}
+const Home= async ({searchParams} : HomeProps)=> {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
+  if(listings.length === 0){
+    return(
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
   return (
-    <div>
-     hello
-    </div>
-   
+    <ClientOnly>
+      <Container>
+        <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+          {
+            listings.map((listing)=>(
+              <ListingCard 
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+              
+            ))
+          }
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
+
+export default Home;

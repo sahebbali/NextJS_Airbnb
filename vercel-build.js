638c82ffca79bed73f6edeefd566017f7e60748c
sahebbali <sahebbali253@gmail.comm>
2023-06-18 15:04:01 +0600

    change

--- vercel-build.js
+++ vercel-build.js
@@ -1,6 +1,17 @@
+// const { execSync } = require('child_process');
+
+// execSync('prisma generate');
+
+// const { nowBuild } = require('@vercel/node');
+// module.exports = nowBuild;
 const { execSync } = require('child_process');
 
-execSync('prisma generate');
+try {
+  execSync('prisma generate');
+} catch (error) {
+  console.error('Error generating Prisma client:', error);
+  process.exit(1); // Exit the build process with a non-zero status code to indicate failure
+}
 
 const { nowBuild } = require('@vercel/node');
 module.exports = nowBuild;
